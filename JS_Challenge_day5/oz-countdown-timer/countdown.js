f605ab4b5a366e9af589bd975bab952f20832faa
Meoyoug <eodyd426@gmail.com>
2023-12-29 18:38:20 +0900

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   JS_Challenge_day5/oz-countdown-timer/.DS_Store
    	modified:   JS_Challenge_day5/oz-countdown-timer/countdown-timer.html
    	modified:   JS_Challenge_day5/oz-countdown-timer/countdown.css
    	modified:   JS_Challenge_day5/oz-countdown-timer/countdown.js

--- JS_Challenge_day5/oz-countdown-timer/countdown.js
+++ JS_Challenge_day5/oz-countdown-timer/countdown.js
@@ -1,9 +1,4 @@
-
-
-//입력받은 날부터 카운트 다운하는 함수
-function Countdown(){
-
-
+function Countdown(){ //카운트다운 객체 생성
     this.timer = null;
     this.isRunning = false;
 
@@ -16,80 +11,61 @@ function Countdown(){
     this.hourDisplay = document.getElementById("hour");
     this.minuteDisplay = document.getElementById("minute");
     this.secondDisplay = document.getElementById("second");
-    this.setTimer = function(){
-        this.time = 0;
-        this.years = 0;
-        this.months = 0;
-        this.days = 0;
-        this.hours = 0;
-        this.minutes = 0;
-        this.seconds = 0;
+    this.box = document.getElementById("timerbox")
 
-        this.updateDisplay()
+    this.prevalues = {
+        sec : null,
+        min : null,
+        h : null,
+        d : null,
+        m : null,
+        y : null
     }
+   
+    this.resetTimer = function(){
+        this.isRunning = false;
+        clearInterval(this.timer);
+        this.seconds = 0
+        this.minutes = 0
+        this.hours = 0
+        this.days = 0
+        this.months = 0
+        this.years = 0
+        document.getElementById("date-select").reset();
+        this.box.style.display = 'none'
+    }
+
     this.startTimer = function(){
+        this.current = new Date()
+        this.time = this.inputDate - this.current;
+
         this.seconds = Math.floor(this.time / 1000) % 60;
         this.minutes = Math.floor(this.time / (1000 * 60)) % 60;
         this.hours = Math.floor(this.time/ (1000 * 60 * 60)) % 24;
         this.days = Math.floor(this.time/ (1000 * 60 * 60 * 24) % 30);
         this.months = Math.floor(this.time/ (1000 * 60 * 60 * 24 *30) % 12); // 단순 예시로 30일을 한 달로 계산합니다.
         this.years = Math.floor(this.time/ (1000 * 60 * 60 * 24 * 30 * 12));
-        if(!this.isRunning){
-            this.isRunning = true;
-            this.timer = setInterval(this.updateCountDown.bind(this), 1000);
-        }
         
-    };
-   
-    this.resetTimer = function(){
-        this.isRunning = false;
-        clearInterval(this.timer);
-        this.years = 0;
-        this.months = 0;
-        this.days = 0;
-        this.hours = 0;
-        this.minutes = 0;
-        this.seconds = 0;
-        console.log(this.years)
-        this.updateDisplay();
-    };
-    this.updateCountDown= function(){
-        this.seconds--; // 시간 감소
-        if (this.seconds === 0 && this.minutes === 0 && this.hours === 0 && this.days === 0 && this.months ===0 & this.years === 0) {
-            clearInterval(this.timer);
-        }
-        if(this.seconds === -1){
-            this.seconds = 59;
-            this.minutes--;
-        }
-        if(this.minutes === -1){
-            this.minutes = 59;
-            this.hours--;                          
-        }
-        if(this.hours === -1){
-            this.hours = 23;
-            this.days--;                          
-        }
-        if(this.days === -1){
-            this.months--;
-            if (this.months === 1 || this.months === 3 || this.months === 5 || this.months === 7 || this.months === 8 || this.months === 10 || this.months === 12) {
-                this.days = 30
-            } else if (this.months === 4 || this.months === 6 || this.months === 9 || this.months === 11) {
-                // 4, 6, 9, 11월은 30일
-                this.days = 29
-            } else {
-                this.days = 27
-            }
-        }
+        this.box.style.display = 'flex'
         
-        if(this.months === -1){
-            this.months = 11;
-            this.years--;
-        }
-        this.updateDisplay();
+        this.addEffect(this.seconds,"sec")
+        this.addEffect(this.minutes,"min")
+        this.addEffect(this.hours,"h")
+        this.addEffect(this.days,"d")
+        this.addEffect(this.months,"m")
+        this.addEffect(this.years,"y")
     }
-        
-
+    this.addEffect = function(value,timeunit){
+        if (value !== this.prevalues[timeunit]) {
+            const display = document.getElementById(timeunit)
+            display.textContent = value
+            display.classList.add('effect');
+            setTimeout(() => {
+                display.classList.remove('effect');
+            }, 500);
+            this.prevalues[timeunit] = value;
+          }
+    }
     this.updateDisplay = function(){  
         this.yearDisplay.textContent = `${this.years} 년`
         this.monthDisplay.textContent = `${this.months} 월`
@@ -106,19 +82,16 @@ function Countdown(){
         let year = event.target.year.value
         let month = event.target.month.value
         let day = event.target.day.value
-        console.log(year)
         
-        const inputDate = new Date(year, month-1, day)
-        const current = new Date()
-        this.time = inputDate - current;
-        console.log(this.time)
+        this.inputDate = new Date(year, month-1, day)
         
-        this.startTimer()
+        if(!this.isRunning){
+            this.isRunning = true;
+            this.timer = setInterval(this.startTimer.bind(this), 1000);
+        }
     }.bind(this))
 }
 
 var countdownTimer = new Countdown();
 
-countdownTimer.setTimer()
-
-//입력받은 년,월,일을 Date 객체로 생성.
+countdownTimer.box.style.display = "none"
